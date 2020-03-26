cd771486393a4d309242a2a44ab1a0fbee9535e7
Dacemmi2mmi2 <46452261+Dacemmi2mmi2@users.noreply.github.com>
2020-03-26 13:41:28 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-var w = window.innerWidth
-|| document.documentElement.clientWidth
-|| document.body.clientWidth;
+// var w = window.innerWidth
+// || document.documentElement.clientWidth
+// || document.body.clientWidth;
 
-var h = window.innerHeight
-|| document.documentElement.clientHeight
-|| document.body.clientHeight;
+// var h = window.innerHeight
+// || document.documentElement.clientHeight
+// || document.body.clientHeight;
 
-var x = document.querySelector('.demo');
-x.innerHTML = "Browser inner window width: " + w + ", height: " + h + ".";
+// var x = document.querySelector('.demo');
+// x.innerHTML = "Browser inner window width: " + w + ", height: " + h + ".";
+
+window.addEventListener('resize', () => {
+    let out = document.querySelector('.demo');
+    out.innerHTML = 'height ' + window.innerHeight + '<br>' + 'width' + window.innerWidth;
+});
